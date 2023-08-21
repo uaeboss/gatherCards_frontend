276ff7da4e2d88cc20368b7f83344292294dafc5
uaeboss <jeskemal@gmail.com>
2023-08-21 17:12:20 +0200

    fetch magic-cards, add card-component

---
+++ src/components/Card.jsx
@@ -0,0 +1,18 @@
+import "./css/Card.css"
+
+const Card = ({ magic }) => {
+  return (
+    <>
+    {/* {magic && 
+      magic.map((cardurl) => {
+        return (
+          <div key={magic.id}>
+              <img className="card_border_radius" src={cardurl.imageUrl} />
+            </div>
+        );
+      })} */}
+    </>
+  );
+};
+
+export default Card;
