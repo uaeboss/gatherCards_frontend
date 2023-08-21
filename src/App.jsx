f4825e3e9be9d9c0f7d512230d05ffccd8dfedf0
uaeboss <jeskemal@gmail.com>
2023-08-21 11:20:59 +0200

    add structure/routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Navigation from "./components/Navigation";
+import Home from "./components/Home.jsx";
+import Footer from "./components/Footer";
+import SideNavigation from "./components/SideNavigation";
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="content_container">
+        <div className="header">
+          <Navigation />
+        </div>
+        <div className="sidenav">
+          <SideNavigation />
+        </div>
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Home />}/>
+            {/* <Route path="magicthegathering" /> */}
+            {/* <Route path="yugioh" /> */}
+          </Routes>
+        </div>
+        <div className="footer">
+          <Footer />
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/SideNavigation.jsx
@@ -0,0 +1,9 @@
+const SideNavigation = () => {
+  return (
+    <>
+      <div>SideNavigation</div>
+    </>
+  );
+};
+
+export default SideNavigation;
