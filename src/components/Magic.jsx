3833382e6a8571684bd83e97547775700a2fbe6c
uaeboss <jeskemal@gmail.com>
2023-08-29 23:52:31 +0200

    api_fetch multiple sets, search function for single cards

--- src/components/Magic.jsx
+++ src/components/Magic.jsx
@@ -2,26 +2,25 @@ import "./css/Magic.css";
 import woe from "../assets/woe.jpg";
 import lotr from "../assets/lotr.jpg";
 import mom from "../assets/mom.png";
+import one from "../assets/one.png";
 import { Link } from "react-router-dom";
 
 const Magic = () => {
   return (
     <>
       <div className="magic_container">
-        <Link to="/magic/woe">
-          <div id="woe">
+        
+        <Link id="woe" to="/magic/woe">
             <img src={woe} width="700px" />
-          </div>
         </Link>
-        <Link to="/magic/ltr">
-          <div id="lotr">
+        <Link id="lotr" to="/magic/ltr">
             <img src={lotr} width="700px" />
-          </div>
         </Link>
-        <Link to="/magic/mom">
-          <div id="mom">
+        <Link id="mom" to="/magic/mom">
             <img src={mom} width="700px" />
-          </div>
+        </Link>
+        <Link id="one" to="/magic/one">
+            <img src={one} width="700px" />
         </Link>
       </div>
     </>
--- src/components/Marketplace.jsx
+++ src/components/Marketplace.jsx
@@ -1,14 +1,19 @@
 import axios from "axios";
-import {stateContext} from '../App.jsx'
-import { useEffect, useState, useContext } from "react";
+import { useEffect, useState } from "react";
 
 export const Marketplace = () => {
-  
+  // const formatUrl = "https://api.scryfall.com/cards/search?include_extras=true&include_variations=true&order=set&q=e%3Awoe&unique=prints";
+
     const [availableCards, setAvailableCards] = useState([]);
 
     useEffect(() => {
       const getAvailableCards = async () => {
         try {
+      //     fetch(formatUrl)
+      // .then((res) => res.json())
+      // .then((allCards) => {
+
+      // });
           const { data } = await axios.get(`${import.meta.env.VITE_BACKENDURL}/cards`);
           setAvailableCards(data);
         } catch (error) {
