9695dff8ec54eed03ef538c5bffcc6361f847c1d
uaeboss <jeskemal@gmail.com>
2023-09-04 11:14:07 +0200

    changes welcome page

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import "./css/LoginForm.css";
 import Loading from "./Loading";
 import { useContext } from "react";
 import { stateContext } from "../context/stateContext.jsx";
@@ -10,9 +11,9 @@ const UserProfile = () => {
 
     return user? (
         <div>
-            <h1>Welcome back, {user.username} - du Nase</h1>
-            <p>Do u want to sell a few cards? <Link to="/auth/create">Sell a card</Link></p>
-            <p>Do want to see the available cards? <Link to="/marketplace">See available cards</Link></p>
+            <h1 id="h1_welcome">Welcome back, {user.username}!</h1>
+            <h2 id="h2_welcome">List your cards for sale: <Link to="/auth/create">Sell a card</Link></h2>
+            <h2 id="h2_welcome">Check out the latest cards in the Marketplace: <Link to="/marketplace">See available cards</Link></h2>
         </div>
     ) : (
         <Loading />
