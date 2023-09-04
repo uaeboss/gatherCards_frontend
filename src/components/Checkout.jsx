bb895fb0200e1dfc1f2027d41bff7fa8ff7be085
uaeboss <jeskemal@gmail.com>
2023-09-04 14:02:15 +0200

    add checkout

---
+++ src/components/Checkout.jsx
@@ -0,0 +1,14 @@
+import purchase from "../assets/purchase.png"
+import "./css/Checkout.css"
+
+export const Checkout = () => { 
+    return (
+        <>
+        <div id="purchase_confirm">
+        <img src={purchase} width="50%"/>
+        <h2 id="checkout_h2">
+Your order has been placed successfully!</h2>
+        </div>
+        </>
+    )
+ }
