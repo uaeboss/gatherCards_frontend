a63ebaeb1a2368d54e1ccb707885caadeb0deebb
uaeboss <jeskemal@gmail.com>
2023-09-05 16:21:56 +0200

    fix

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,22 @@
+import React, { useState } from "react";
 import "./css/Footer.css";
 import facebook from "../assets/facebook.png";
 import twitter from "../assets/twitter.png";
 import instagram from "../assets/instagram.png";
+import { toast } from "react-toastify";
 
 const Footer = () => {
+  const [email, setEmail] = useState(""); // State to manage the input field value
+
+  const onNewsletter = () => {
+    if (email) {
+      toast("Awesome! You have signed up for our newsletter 🥳");
+      setEmail("");
+    } else {
+      toast("Please enter your email address.");
+    }
+  };
+
   return (
     <>
       <div className="footer_width">
@@ -21,25 +34,31 @@ const Footer = () => {
         </div>
         <div className="footer_mid">
           <div id="newsletter">
-            <input type="email" placeholder="Subscribe to our newsletter" />
-            <button>Subscribe</button>
+            <input
+              id="nw_lt"
+              type="email"
+              placeholder="Subscribe to our newsletter"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)} // Update email state
+            />
+            <button onClick={onNewsletter}>Subscribe</button>
           </div>
         </div>
         <div className="footer_right">
           <div id="socialmedia_images">
             <div id="facebook">
               <a href="https://de-de.facebook.com/">
-                <img src={facebook} />
+                <img src={facebook} alt="Facebook" />
               </a>
             </div>
             <div id="instagram">
               <a href="https://www.instagram.com/">
-                <img src={instagram} width="60px" />
+                <img src={instagram} alt="Instagram" width="60px" />
               </a>
             </div>
             <div id="twitter">
               <a href="https://twitter.com/">
-                <img src={twitter} />
+                <img src={twitter} alt="Twitter" />
               </a>
             </div>
           </div>
