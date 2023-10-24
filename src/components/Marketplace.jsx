a95100d4716224c46726b12a02c125a1d7de4e9a
uaeboss <jeskemal@gmail.com>
2023-10-24 15:44:53 +0200

    add loading to marketplace

--- src/components/Marketplace.jsx
+++ src/components/Marketplace.jsx
@@ -3,9 +3,11 @@ import "./css/Marketplace.css";
 import { useEffect, useState } from "react";
 import { MarketplaceCards } from "./MarketplaceCards";
 import { toast } from "react-toastify";
+import Loading from "./Loading";
 
 export const Marketplace = () => {
   const [availableCards, setAvailableCards] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const getAvailableCards = async () => {
@@ -14,8 +16,10 @@ export const Marketplace = () => {
           `${import.meta.env.VITE_BACKENDURL}/cards`
         );
         setAvailableCards(data);
+        setLoading(false);
       } catch (error) {
         toast.error(error);
+        setLoading(false);
       }
     };
     getAvailableCards();
@@ -24,18 +28,22 @@ export const Marketplace = () => {
   return (
     <>
       <div id="marketplace_container">
-        {availableCards?.map((card) => (
-          <div key={card._id}>
-            <MarketplaceCards
-              cardId={card.id}
-              name={card.name}
-              image={card.image}
-              qty={card.qty}
-              price={card.price}
-              seller={card.seller.username}
-            />
-          </div>
-        ))}
+        {loading ? (
+          <Loading />
+        ) : (
+          availableCards?.map((card) => (
+            <div key={card._id}>
+              <MarketplaceCards
+                cardId={card.id}
+                name={card.name}
+                image={card.image}
+                qty={card.qty}
+                price={card.price}
+                seller={card.seller.username}
+              />
+            </div>
+          ))
+        )}
       </div>
     </>
   );
